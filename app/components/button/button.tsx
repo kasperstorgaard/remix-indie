1476c6d6638ecf6a2b105ec36536e205be57a0a0
kasperstorgaard <kasper.storgaard@gmail.com>
2022-05-12 09:41:07 +0200

    feat(components): add button variations

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -6,13 +6,21 @@ export const links = () => [
 ];
 
 interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  type?: 'submit'|'reset'|'button'
+  type?: 'submit'|'reset'|'button',
+  variation?: 'primary'|'secondary'|'error'|'warn',
+  inverted?: boolean,
 } 
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, ...props }, ref) => {
+    const classNames = [
+      'ri-button',
+      props.variation && `-${props.variation}`,
+      props.inverted && '-inverted'
+    ].join(' ');
+
     return (
-      <button {...props} ref={ref} className="ri-button">
+      <button {...props} ref={ref} className={classNames}>
         { children }
       </button>
     );
