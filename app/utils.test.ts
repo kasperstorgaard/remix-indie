26ec95aa9497c33231bc068be162ef5872c25264
kasperstorgaard <kasper.storgaard@gmail.com>
2022-05-15 09:14:50 +0200

    test(utils): add unit test for redirect validation

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,4 +1,4 @@
-import { validateEmail } from "./utils";
+import { validateEmail, validateRedirect } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
   expect(validateEmail(undefined)).toBe(false);
@@ -10,4 +10,14 @@ test("validateEmail returns false for non-emails", () => {
 
 test("validateEmail returns true for emails", () => {
   expect(validateEmail("kody@example.com")).toBe(true);
+});
+
+test("validateRedirect returns false for non relative paths", () => {
+  expect(validateRedirect("https://www.foo.com")).toBe(false);
+  expect(validateRedirect("www.foo.com")).toBe(false);
+  expect(validateRedirect("foo.com")).toBe(false);
+});
+
+test("validateRedirect returns true for relative paths", () => {
+  expect(validateRedirect("/login")).toBe(true);
 });
